c261d22f336a952ac24b9eeecd27926a48c8dfcd
Python Full-stack <koses910@gmail.com>
2025-01-11 12:37:23 +0530

    3

---
+++ Q.3/src/store.jsx
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import myReducer from "./colorSlice";
+const store=configureStore({
+    reducer:{
+        mycolor:myReducer
+    }
+})
+export default store;
